38082524daa8c0cf5d33b43b94aed8c776a0846f
Hadrien Mens-Pellen <h.menspellen@gmail.com>
2013-05-06 23:05:34 +0200

    Ajout d'un script de déploiement automatique

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Récupération du code par git..."
+git pull
+echo "Récupération terminée."
+echo "Renommage du Loader sparks"
+mv application/core/My_Loader.php application/core/MY_Loader.php
+
+export file=htdocs/index.php
+sed "s/'[A-Z]:\\.+codeigniter\/(['^]+)'/'/kunden/homepages/43/d383453256/htdocs/hadrienmp/$1'/g" "$file" > "$file".tmp && mv -f "$file".tmp "$file"
